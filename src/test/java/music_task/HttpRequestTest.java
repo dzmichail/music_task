41801d1905c472160889fe5605c7c3eb4e42acef
Michail.Shirokov <mihail.shirokov@dz.ru>
2018-10-08 18:24:58 +0500

    fourth commit, testing was added

---
+++ src/test/java/music_task/HttpRequestTest.java
@@ -0,0 +1,27 @@
+package music_task;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import app.App;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = App.class)
+public class HttpRequestTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void controllerResponse() throws Exception{
+        assertThat(this.testRestTemplate.getForObject("http://localhost:" + port + "/" + "get/Hello", String.class)).startsWith("Hello");
+    }
+}
